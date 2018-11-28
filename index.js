348a0dbcf786f96a9390f2fa5afaa84fe5037bb8
hamyongjae <bbb12388@naver.com>
2018-11-28 09:33:58 +0900

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const express = require('express'); 
+require('./services/passport');
+
+const User = require('./models/User');
+
+const auth = require('./routes/auth');
+const users = require('./routes/users'); // 추가
+
+const keys = require('./config/default');
+
+const cookieSession = require('cookie-session');
+const passport = require('passport'); 
+const mongoose = require('mongoose');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ happy: 'hacking '});
-});
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error(error.message));
 
-// Dynamic Port binding
+app.use(
+  cookieSession({
+    name: 'MERN cookie',
+    maxAge: (30 * 24 * 60 * 60 * 1000),
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/auth/google', auth);
+app.use('/api/users', users); // 추가
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {console.log('hi')});
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
